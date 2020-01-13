a0c314722b295674ae5468011e846ee31ecdb41e
julianyraiol <jrr.snf@uea.edu.br>
2020-01-13 19:27:22 -0400

    refactor: change structure to OOP
    - add class GuarujaCrawler, GuarujaDriver

--- crawler.py
+++ crawler.py
@@ -7,47 +7,75 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
-delay = 2
-logging.basicConfig(level = logging.INFO)
-
-driver = webdriver.Firefox()
-driver.get("http://www.guaruja.sp.gov.br/edicoes-diario-oficial/")
-
-elementyears = driver.find_element_by_id("mec_sf_year_22564")
-elementmonth = driver.find_element_by_id("mec_sf_month_22564")
- 
 months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
-days = driver.find_elements_by_class_name("mec-calendar-day")
-years = [x.get_attribute("value") for x in elementyears.find_elements_by_tag_name("option")]
+delay = 3
 
-select = Select(elementyears)
-select1 = Select(elementmonth)
+class GuarujaDriver(object):
+    def __init__(self, url):
+        self.driver = webdriver.Firefox()
+        self.driver.get(url)
+        logging.basicConfig(level = logging.INFO)
+    
+    def get_element_by_id(self, element_id):
+        return self.driver.find_element_by_id(element_id)
+    
+    def get_elements_by_class(self, element_class):
+        return self.driver.find_elements_by_class_name(element_class)
+    
+    def get_options_by_id(self, element):
+        element_id = Select(element)
+        return [x.get_attribute("value") for x in element_id.options]
 
-driver.execute_script("window.scrollBy(0, 400)")
-time.sleep(delay)
-calendar = driver.find_elements_by_class_name("mec-color-hover") 
-
-for days in calendar:
-    try:    
-        filelink = days.get_attribute("href")
-        request_file = requests.get(filelink)
-        namefile = days.get_attribute('text').replace("/", "-") + ".pdf"
-        with open(namefile, "wb") as filepdf:
-            filepdf.write(request_file.content)
-        logging.info("Arquivo baixado")
-    except: 
-        logging.warning("Falha ao fazer download dos arquivos")
-        
-'''
-for year in years:
-    select.select_by_value(year)
-    for month in months:
-        select1.select_by_visible_text(month)
+    def select_by_text(self, element, text):
+        select = Select(element)
+        select.select_by_visible_text(text)
         time.sleep(delay)
-        #add codigo aqui
-    driver.execute_script("window.scrollBy(0, 0)")
-    time.sleep(delay)
-'''
-driver.close()
 
+    def select_by_value(self, element, value):
+        select = Select(element)
+        select.select_by_value(value)
 
+    def go_to_calendar(self):
+        self.driver.execute_script("window.scrollBy(0, 400)")
+
+class GuarujaCrawler(object):
+    def __init__(self):
+        self.driver = GuarujaDriver("http://www.guaruja.sp.gov.br/edicoes-diario-oficial/")
+
+    def download_and_save_file(self, filelink, filename):
+        try:
+            requestlink = requests.get(filelink)
+            with open(filename, "wb") as f:
+                f.write(requestlink.content)
+            logging.info("Arquivo baixado: " + filename)
+        except:
+            logging.warning("Falha ao fazer download dos arquivos")
+
+    def get_files_by_day(self):
+        calendar = self.driver.get_elements_by_class("mec-color-hover")
+        self.driver.go_to_calendar()
+
+        for days in calendar:
+            filelink = days.get_attribute("href")
+            filename = days.get_attribute('text').replace("/", "-") + ".pdf"
+
+            self.download_and_save_file(filelink, filename)
+    
+    def set_filter_by_month_year(self):
+        selectyear = self.driver.get_element_by_id("mec_sf_year_22564")
+        selectmonth = self.driver.get_element_by_id("mec_sf_month_22564")
+
+        years = self.driver.get_options_by_id(selectyear)
+
+        for year in years:
+            self.driver.select_by_text(selectyear, year)
+        
+            for month in months:
+                self.driver.select_by_text(selectmonth, month)
+                self.get_files_by_day()                
+                self.driver.driver.execute_script("window.scrollBy(0, 0)")
+      
+if __name__ == "__main__":
+    crawler = GuarujaCrawler()
+    crawler.set_filter_by_month_year()
+    crawler.driver.driver.close()
